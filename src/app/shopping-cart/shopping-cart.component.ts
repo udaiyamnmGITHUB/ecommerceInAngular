d2bca070df09d3394777ca612bead7f6259dc6e9
Udai <udaiyamnm@gmail.com>
2021-05-25 09:31:10 +0100

    discount applied

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 
-import { selectProducts} from '../state/product.selectors';
+import { selectProducts } from '../state/product.selectors';
 import { DataService } from '../services/data.service';
 import { ShoppingCartItem, OrderInfo, ProductInfo } from '../interface/ec-template.interface';
 
@@ -14,45 +14,68 @@ export class ShoppingCartComponent implements OnInit {
   data: ShoppingCartItem[] = [];
   // order summary
   subTotal = 0;
-  shippingFee = 50;
-  taxPercentage = 5; // 5%
-  tax = 0;
+  shippingFee = 0;
   total = 0;
+  discount = 0;
   products$ = this.store.pipe(select(selectProducts));
   prodListList: ProductInfo[];
-  constructor(private dataService: DataService, private store: Store<any>) {}
+  constructor(private dataService: DataService, private store: Store<any>) { }
 
   ngOnInit() {
     this.data = this.dataService.shoppingCartData;
     this.products$.subscribe(prodList => {
       this.prodListList = prodList;
     });
-    this.getOrderSummary();
+    this.getOrderSummary(0);
   }
 
   removeItem(item: ShoppingCartItem) {
     this.dataService.deleteShoppingCartItem(item, this.prodListList);
     this.data = this.dataService.shoppingCartData;
-    this.getOrderSummary();
+    this.getOrderSummary(0);
   }
 
   getTotalPrice(item: ShoppingCartItem) {
-    return item.quantity * +item.product.costPrice;
+    let totalPrice = item.quantity * +item.product.costPrice;
+    return Math.round(totalPrice * 100) / 100;
   }
 
-  getOrderSummary() {
+  applyDiscount(voucherCode: string) {
+    switch (voucherCode) {
+      case "BOH232":
+        {
+          let discountCanBeApplied = 5;
+          this.getOrderSummary(discountCanBeApplied);
+        }
+      case "BOH233":
+        {
+          /* yet to implement */
+        }
+
+      case "BOH234":
+        {
+          /* yet to implement */
+        }
+    }
+  }
+
+  getOrderSummary(discount: number) {
     this.subTotal = 0;
     for (const i of this.data) {
       this.subTotal = this.subTotal + +i.product.costPrice * i.quantity;
     }
-    this.tax = (this.subTotal * this.taxPercentage) / 100;
-    this.total = this.subTotal + this.shippingFee + this.tax;
+    this.total = this.subTotal + this.shippingFee;
+    if (discount) {
+      this.discount = discount;
+      this.total = this.total - discount;
+    }
+  }
+
+  scrollToTop() {
+    window.scroll(0, 0);
   }
 
   onCheckOut() {
-    this.dataService.saveOrderInfo(<OrderInfo>{
-      items: this.data,
-      totalPrice: this.total
-    });
+    /* to payment module */
   }
 }
